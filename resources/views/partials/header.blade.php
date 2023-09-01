697a1dd5d0e11c414d53a046b31effb56fd76caf
valdir169 <contrerasvaldir06@gmail.com>
2023-08-31 23:33:39 -0500

    create header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,88 @@
+<header class="navbar ">
+    <div class="w-16 h-full">
+        <img src={{ asset('images/logo.png') }} alt="Iespp Huamachuco" class="w-full h-full">
+    </div>
+
+    <nav class="w-full absolute nav-menu">
+        <ul class="flex flex-col h-full center items-center px-14 md:flex-row md:gap-2 lg:gap-5">
+            <li class="nav-link">
+                <a href="">Inicio</a>
+            </li>
+
+            <li class="prueba nav-link ">
+                <a class="w-full flex justify-between">Institución
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+
+                <ul class="submenu deplegable">
+                    <li><a href="https://isthuamachuco.edu.pe/programas-estudio/computacion-e-informatica/">Presentación</a></li>
+                    <li><a href="#">Historia</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Organigrama</a></li>
+                    <li><a href="#">Directorio</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-link">
+                <a class="w-full flex justify-between">Admisión
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+
+                <ul class="submenu">
+                    <li><a href="">Admisión 2024</a></li>
+                    <li><a href="">Becas y Creditos</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-link">
+                <a class="w-full flex justify-between">Carreras
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+                <ul class="submenu">
+                    <li><a href="#">Ingles</a></li>
+                    <li><a href="#">Educación Inicial</a></li>
+                    <li><a href="#">Comunicación</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-link">
+                <a class="w-full flex justify-between">Servicios
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+
+                <ul class="submenu">
+                    <li><a href="#">Laboratorio</a></li>
+                    <li><a href="#">Biblioteca</a></li>
+                    <li><a href="#">Tópico</a></li>
+                    <li><a href="#">Psicopedagogia</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-link">
+                <a class="w-full flex justify-between">Tramites
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+
+                <ul class="submenu">
+                    <li><a href="#">Trámite documentario</a></li>
+                    <li><a href="#">Contáctnos</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-link">
+                <a class="w-full flex justify-between">Otros
+                    <i class='bx bx-chevron-down'></i>
+                </a>
+
+                <ul class="submenu">
+                    <li><a href="#">Galeria</a></li>
+                    <li><a href="#">Eva</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
+    <button class="btn-menu" type="button" >
+        <i class='bx bx-menu' ></i>
+    </button>
+</header>

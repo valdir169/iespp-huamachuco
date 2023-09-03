15bd09f36f78a77223affcdbda09001ca61b361d
Valdir Contreras <114438634+valdir169@users.noreply.github.com>
2023-09-02 22:35:10 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
 Route::get('admision', function(){
     return view('pages.admision');
 });

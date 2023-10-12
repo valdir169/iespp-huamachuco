137c35fbbc844f700e29dccf57ef9afadd5c897e
AntonyJ <reyesantony.452@gmail.com>
2023-10-12 13:47:48 -0500

    update page servicios section topico

--- resources/views/pages/servicios/prevencion_riesgos.blade.php
+++ resources/views/pages/servicios/prevencion_riesgos.blade.php
@@ -1,1 +1,11 @@
-<h1>Prenvencion de riesgos ambientales</h1>
+@extends('layout.app')
+
+@section('title', 'Capacitacion de prevencion de riesgos ambientales')
+
+@section('content')
+<x-hero image="{{ asset('images/slider/slider4.jpg') }}" text="Capacitacion de Riesgos Ambientales" />
+
+<div class="py-16">
+    <h1>Hola a todos</h1>
+</div>
+@endsection
